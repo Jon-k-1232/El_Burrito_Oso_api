024e4e9db6c03a368c54543d23d0d03f01ee1527
Jon Kimmel <jonkimmel16@gmail.com>
2020-03-01 22:55:27 -0700

    v1

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://@localhost/el_burrito_oso',
 };
 
