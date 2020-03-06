bb2a412edc309cf9ddd6e6ae2ef4a15b6e217923
Jon Kimmel <jonkimmel16@gmail.com>
2020-03-05 21:43:47 -0700

    test update

--- test/app.test.js
+++ test/app.test.js
@@ -1,66 +1,54 @@
-const chai = require('chai')
-let chaiHttp = require('chai-http');
-const app = require('../src/app.js');
+const chai = require("chai");
+let chaiHttp = require("chai-http");
+const app = require("../src/app.js");
 const should = chai.should();
-const { TEST_DATABASE_URL, API_TOKEN2 } = require('../src/config.js');
-const  knex = require('knex');
-
-
-
-
+const { TEST_DATABASE_URL, API_TOKEN2 } = require("../src/config.js");
+const knex = require("knex");
 
 chai.use(chaiHttp);
 
+describe("Express App", () => {
+  let db;
 
-
-
-describe('Express App', () => {
-
-    let db;
-
-    before('make knex instance', () => {
-        db = knex({
-                client: 'pg',
-                connection: TEST_DATABASE_URL,
-            });
-        return app.set('db', db)
+  before("make knex instance", () => {
+    db = knex({
+      client: "pg",
+      connection: TEST_DATABASE_URL
     });
+    return app.set("db", db);
+  });
 
+  it("should return a 200 status message from POST /", () => {
+    // Testing POST
+    let fakeData = {
+      restaurantID: "123456",
+      review: "this is a test of the post route",
+      rating: 5.5
+    };
 
+    chai
+      .request(app)
+      .post("/reviews/submit")
+      .set("Authorization", `Bearer ${API_TOKEN2}`)
+      .send(fakeData)
+      .end((err, res) => {
+        res.should.have.status(200);
+        should.exist(res.body);
+        res.body.should.be.a("object");
+      });
+  });
 
-    it('should return a 200 status message from POST /', () => {                // Testing POST
-        let fakeData = {
-            restaurantID: '123456',
-            review: 'this is a test of the post route',
-            rating: 5.5,
-        };
-
-        chai.request(app)
-            .post('/reviews/submit',{
-                headers: {
-                    Authorization: `Bearer ${API_TOKEN2}`
-                }
-            })
-            .send(fakeData)
-            .end((err,res)=>{
-                res.should.have.status(200);
-                should.exist(res.body);
-                res.body.should.be.a('object')
-            })
-    });
-
-
-
-    it.only('should return a message from GET /', (done) => {               // Testing GET
-        chai.request(app)
-            .get('/locations/16421+North+tatum+Blvd,+Glendale,+Az,+85032')
-            .set('Authorization',`Bearer ${API_TOKEN2}`)
-            .end((err, res) => {
-                res.should.have.status(200);
-                should.exist(res.body);
-                res.body.should.be.a('object');
-                done();
-            });
-    });
+  it("should return a message from GET /", done => {
+    // Testing GET
+    chai
+      .request(app)
+      .get("/locations/16421+North+tatum+Blvd,+Glendale,+Az,+85032")
+      .set("Authorization", `Bearer ${API_TOKEN2}`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        should.exist(res.body);
+        res.body.should.be.a("object");
+        done();
+      });
+  });
 });
-
